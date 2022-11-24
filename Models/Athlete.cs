36c9f2d1984fc7e4e3dae94feb40d06661c01b06
Romane Cote-colisson <rcotecoliss@ensc.fr>
2022-11-24 11:35:49 +0100

    update class

--- Models/Athlete.cs
+++ Models/Athlete.cs
@@ -3,8 +3,7 @@ public class Athlete
     public int Id { get; set; }
     public string LastName { get; set; } = null!;
     public string FirstName { get; set; } = null!;
-    public int Age { get; set; }
-    public string BriefDescription { get; set; } = null!;
+    public DateTime BirthDate { get; set; }
     public string Nationality { get; set; } = null!;
     public Discipline Discipline { get; set; } = null!;
 
--- Models/Sport.cs
+++ Models/Sport.cs
@@ -2,6 +2,6 @@ public class Sport
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
+    public char Gender { get; set; }
     public List<Discipline> Disciplines { get; set; } = null!;
 }
