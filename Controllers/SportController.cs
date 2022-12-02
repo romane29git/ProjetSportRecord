09f57767f8a00c5d5da7f26bb3185fa371927ae2
Romane Cote-colisson <rcotecoliss@ensc.fr>
2022-12-02 16:25:01 +0100

    ajout controller/vues

---
+++ Controllers/SportController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+public class SportController : Controller // not ControllerBase!
+{
+
+    private readonly SportRecordContext _context;
+
+    public SportController(SportRecordContext context)
+    {
+        _context = context;
+    }
+
+    // GET: /Hello/
+    public async Task<IActionResult> Index()
+    {
+        var sports = await _context.Sports
+        .OrderBy(s => s.Name)
+        .ToListAsync();
+
+        return View(sports);
+    }
+
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var sports = await _context.Sports
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (sports == null)
+        {
+            return NotFound();
+        }
+        return View(sports);
+    }
+
+
+}
+
+
