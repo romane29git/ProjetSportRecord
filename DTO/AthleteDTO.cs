0f04cae378515af7559834c5967e3151835b8af1
Célia Ribard Del Castillo <celia.rdc31@gmail.com>
2022-12-09 16:31:02 +0100

    ajout athleteDTO

---
+++ DTO/AthleteDTO.cs
@@ -0,0 +1,12 @@
+// Data Transfer Object class
+
+public class AthleteDTO
+{
+    public int Id { get; set; }
+    public string LastName { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public char Gender { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Nationality { get; set; } = null!;
+    public int DisciplineId { get; set; }
+}
--- Models/Athlete.cs
+++ Models/Athlete.cs
@@ -8,4 +8,16 @@ public class Athlete
     public string Nationality { get; set; } = null!;
     public Discipline? Discipline { get; set; } = null!;
 
+    // Constructeur vide par défaut
+    public Athlete() { }
+
+    public Athlete(AthleteDTO dto)
+    {
+        Id = dto.Id;
+        LastName = dto.LastName;
+        FirstName = dto.FirstName;
+        Gender = dto.Gender;
+        BirthDate = dto.BirthDate;
+        Nationality = dto.Nationality;
+    }
 }
