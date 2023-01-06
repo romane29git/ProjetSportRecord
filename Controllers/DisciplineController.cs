f9c0c9c342d6ddba13f670aa839510750e71f3ce
Celia <celia.rdc31@gmail.com>
2023-01-06 14:18:15 +0100

    ajout page disciplines

---
+++ Controllers/DisciplineController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+public class DisciplineController : Controller // not ControllerBase!
+{
+    private readonly SportRecordContext _context;
+
+    public DisciplineController(SportRecordContext context)
+    {
+        _context = context;
+    }
+
+    // public IActionResult Create()
+    // {
+    //     return View();
+    // }
+
+    // GET: Discipline
+    public async Task<IActionResult> Index()
+    {
+        var disciplines = await _context.Disciplines
+        .OrderBy(d => d.Name)
+        .Where(d => d.Id == 1)
+        .ToListAsync();
+
+        return View(disciplines);
+    }
+}
--- Controllers/SportController.cs
+++ Controllers/SportController.cs
@@ -12,12 +12,10 @@ public class SportController : Controller // not ControllerBase!
         _context = context;
     }
 
-    // GET: Sport
     public IActionResult Create()
     {
         return View();
     }
-
 
     // GET: Sport
     public async Task<IActionResult> Index()
@@ -29,35 +27,8 @@ public class SportController : Controller // not ControllerBase!
         return View(sports);
     }
 
-    public async Task<IActionResult> Details(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var sports = await _context.Sports
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (sports == null)
-        {
-            return NotFound();
-        }
-        return View(sports);
-    }
-
-    // // POST: Sport/Create
-    // [HttpPost]
-    // [ValidateAntiForgeryToken]
-    // public async Task<IActionResult> Create(Sport sport)
-    // {
-    //     // Create new sport in DB
-    //     _context.Add(sport);
-    //     await _context.SaveChangesAsync();
-
-    //     // Redirect to student details
-    //     return RedirectToAction("Sport");
-    // }
-
+    
+    // POST: Sport/Create
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Create([Bind("Id,Name")] Sport sport)
