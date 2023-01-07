80b5efba156da975baf85da94fdc0d4ad8970f39
Romane Cote-colisson <rcotecoliss@ensc.fr>
2023-01-07 10:11:22 +0100

    ajout controller create discipline

--- Controllers/SportController.cs
+++ Controllers/SportController.cs
@@ -83,6 +83,74 @@ public class SportController : Controller // not ControllerBase!
         return View(sport);
     }
 
+
+
+    // GET: Sport/CreateDiscipline?SportId=5
+    public async Task<IActionResult> CreateDiscipline(int? sportId)
+    {
+        if (sportId == null)
+        {
+            return NotFound();
+        }
+
+        // Lookup sports by id
+        var sport = await _context.Sports.FindAsync(sportId);
+        if (sport == null)
+        {
+            return NotFound();
+        }
+
+        // Retrieve list of sports existing
+        var sportsQuery = from s in _context.Sports
+                          select s;
+
+        var availableSports = sportsQuery.ToList();
+
+        ViewData["Sports"] = sport;
+        //ViewData["CourseId"] = new SelectList(availableCourses, "Id", "Title");
+        return View();
+    }
+
+
+
+    // POST: Sport/CreateDiscipline
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateDiscipline([Bind("Id,Name,SportId")] Discipline discipline)
+    {
+
+
+        // Apply model validation rules
+        if (ModelState.IsValid)
+        {
+            _context.Add(discipline);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        // At this point, something failed: redisplay form
+        return View(discipline);
+
+
+
+        // Lookup student and course
+        // var student = _context.Students.Find(enrollment.StudentId);
+        // var course = _context.Courses.Find(enrollment.CourseId);
+
+        // // Define student and course for new enrollment
+        // enrollment.Student = student!;
+        // enrollment.Course = course!;
+
+        // // Create new enrollment in DB
+        // _context.Add(enrollment);
+        // await _context.SaveChangesAsync();
+
+        // // Redirect to student details
+        // return RedirectToAction("Details", "Student", new RouteValueDictionary { { "id", enrollment.StudentId } });
+
+
+
+    }
+
 }
 
 
